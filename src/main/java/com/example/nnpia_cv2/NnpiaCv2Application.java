5ef4732b9d1ed7218767859493b1e50034af6c08
Petr Jiruše <petrjiruse@seznam.cz>
2024-02-20 14:18:40 +0100

    initial commit

---
+++ src/main/java/com/example/nnpia_cv2/NnpiaCv2Application.java
@@ -0,0 +1,13 @@
+package com.example.nnpia_cv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaCv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnpiaCv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nnpia_cv2/NnpiaCv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nnpia_cv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaCv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
