0568dfd7f2cf52e9d97fbc7975b6927c3cd790a5
Petr Jiruše <petrjiruse@seznam.cz>
2024-02-20 14:58:53 +0100

    Add endpoint with path variable

--- src/main/java/com/example/nnpia_cv2/controller/HelloController.java
+++ src/main/java/com/example/nnpia_cv2/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.nnpia_cv2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1")
 @RestController
 public class HelloController {
 
     @GetMapping("")
     public String helloWorld(){
         return "Hello World from Spring Boot application";
+    }
+
+    @GetMapping("/{string}")
+    public String helloWorldWithParameter(@PathVariable("string") String string){
+        return "Received string: " + string;
     }
 }
