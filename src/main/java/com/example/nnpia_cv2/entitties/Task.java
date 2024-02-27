3ef5ed9e3cea79a8a561d00f3bac72a019bbe48e
Petr Jiruše <petrjiruse@seznam.cz>
2024-02-27 16:00:03 +0100

    Add Task class

---
+++ src/main/java/com/example/nnpia_cv2/entitties/Task.java
@@ -0,0 +1,28 @@
+package com.example.nnpia_cv2.entitties;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String description;
+    @Column
+    private LocalDateTime creation_date;
+    @Column
+    private LocalDateTime update_date;
+    @Column
+    private Long author_id;
+}
