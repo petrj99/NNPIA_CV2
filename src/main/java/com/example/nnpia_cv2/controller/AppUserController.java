f81e1a913116f87fc0f84e3ca0f317daf548919a
Petr Jiruše <petrjiruse@seznam.cz>
2024-03-05 15:20:00 +0100

    Add PUT mapping and DELETE mapping to AppUserController

--- src/main/java/com/example/nnpia_cv2/controller/AppUserController.java
+++ src/main/java/com/example/nnpia_cv2/controller/AppUserController.java
@@ -1,6 +1,7 @@
 package com.example.nnpia_cv2.controller;
 
 import com.example.nnpia_cv2.entitties.AppUser;
+import com.example.nnpia_cv2.entitties.AppUserRole;
 import com.example.nnpia_cv2.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,5 +32,31 @@ public class AppUserController {
     public ResponseEntity<AppUser> createUser(@RequestBody AppUser newUser){
         AppUser savedUser = appUserRepository.save(newUser);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AppUser> updateUser(@PathVariable Long id, @RequestBody AppUser userDetails){
+        return appUserRepository.findById(id)
+                .map(existingUser -> {
+                    existingUser.setUsername(userDetails.getUsername());
+                    existingUser.setPassword(userDetails.getPassword());
+                    existingUser.setActive(userDetails.getActive());
+                    existingUser.setCreationDate(userDetails.getCreationDate());
+                    existingUser.setUpdateDate(userDetails.getUpdateDate());
+
+                    AppUser updatedUser = appUserRepository.save(existingUser);
+                    return ResponseEntity.ok(updatedUser);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        return appUserRepository.findById(id)
+                .map(user -> {
+                    appUserRepository.delete(user);
+                    return ResponseEntity.ok().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
